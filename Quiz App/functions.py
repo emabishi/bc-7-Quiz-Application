df6b38aff4185fe7e24f477bd3e54535c7bc31b0
Elizabeth Mabishi <emabishi@yahoo.com>
2016-05-02 18:00:29 +0300

    Add quiz functions

--- Quiz App/functions.py
+++ Quiz App/functions.py
@@ -55,7 +55,7 @@ def list_quizzes():
 	print("\nTip: Use command 'takequiz<quizname>' to begin taking a quiz\n")
 
 	#Add some styling
-	print("\n" + "$" *20 + "=" * 20 + "%" * 20 + "\n")
+	print("$" *20 + "=" * 20 + "%" * 20)
 
 def rank():
 	level_response = input("Please choose your prefered player level.\n Type 'B' or 'b' for Beginner Mode\n Type 'I' or 'i' for intermediate Mode and\n Type 'G' or 'g' for Guru Mode")
@@ -91,64 +91,18 @@ def rank():
 
 #takequiz <quiz_name> - Start taking a new quiz
 def take_quiz(quiz_name):
-	quiz_name = input("\n Use command 'takequiz<quizname>' to begin taking a quiz\n")    
+	quiz_name = input("Use command 'takequiz<quizname>' to begin taking a quiz\n")    
 
 	#name of local json file. get base name as quiz name
-	path_to_quiz_LOTR = 'C:\\Quizzler\\Quizzes\\LOTR'
-	if quiz_name == os.path.basename(path_to_quiz_LOTR):
+	path_to_quiz_LOTR = 'C:\\Quizzler\\Quizzes\\LOTR.json'
+	if quiz_name in os.path.basename(path_to_quiz_LOTR):
 
 		#use json load function to convert to list
 		with open(path_to_quiz_LOTR) as LOTR_quiz:
-			LOTR_questions = json.load(LOTR_quiz)
+			LOTR = json.load(LOTR_quiz)
 		
 		#run LOTR quiz----- #1. Shuffle questions in quiz
-		questions = LOTR_questions.keys()
-		random.shuffle(questions)
-
-		#Start quiz
-		#Set initial score to Zero 
-		score = 0
-
-		#Monitor number of questions asked
-		position = 0
-		while position < len(LOTR_questions):
-
-			#return a question in the quiz
-			print LOTR_questions[position]
-
-			#Call start timing function
-			#start_timing()
-
-			#Prompt user for an answer
-			user_answer = input("Please enter your answer.\n")
-			if user_answer == LOTR_questions[position]:
-				print("Your answer is correct! \n")
-				score += 1
-				print("Your score is {}").format(score)
-			else:
-				print("Your answer is incorrect \n")
-				print("Your score is {}").format(score)
-			#A question has been attempted, increment position variable
-			position +=1
-			if position == len(LOTR_questions):
-				print("Your total socre is {} \n").format(score)
-				print("Questions in module over. Please take another quiz")
-
-				#Call rank function
-				rank()
-
-				#Call list quizzes function
-				list_quizzes()
-
-
-	path_to_quiz_GOT = 'C:\\Quizzler\\Quizzes\\GOT'
-	if quiz_name == os.path.basename(path_to_quiz_GOT):
-		#use json load function to convert to list
-		with open(path_to_quiz_GOT) as GOT_quiz:
-			GOT_questions = json.load(GOT_quiz)
-
-		#run quiz
-		questions  = GOT_questions.keys()
+		questions = LOTR.keys()
 		random.shuffle(questions)
 
 		#Start quiz
@@ -167,7 +121,7 @@ def take_quiz(quiz_name):
 
 			#Prompt user for an answer
 			user_answer = input("Please enter your answer.\n")
-			if user_answer == questions[position]:
+			if user_answer == LOTR[(questions[position])]:   #Answer to question
 				print("Your answer is correct! \n")
 				score += 1
 				print("Your score is {}").format(score)
@@ -185,17 +139,18 @@ def take_quiz(quiz_name):
 
 				#Call list quizzes function
 				list_quizzes()
+	else:
+		print "Invalid response.Quiz does not exist."
 
 
-	path_to_quiz_MATH = 'C:\\Quizzler\\Quizzes\\MATH'
-	if quiz_name == os.path.basename(path_to_quiz_MATH):
-
+	path_to_quiz_GOT = 'C:\\Quizzler\\Quizzes\\GOT'
+	if quiz_name in os.path.basename(path_to_quiz_GOT):
 		#use json load function to convert to list
-		with open(path_to_quiz_MATH) as MATH_quiz:
-			GOT_questions = json.load(GOT_quiz)
+		with open(path_to_quiz_GOT) as GOT_quiz:
+			GOT = json.load(GOT_quiz)
 
 		#run quiz
-		questions  = GOT_questions.keys()
+		questions  = GOT.keys()
 		random.shuffle(questions)
 
 		#Start quiz
@@ -214,7 +169,55 @@ def take_quiz(quiz_name):
 
 			#Prompt user for an answer
 			user_answer = input("Please enter your answer.\n")
-			if user_answer == questions[position]:
+			if user_answer == GOT[(questions[position])]:
+				print("Your answer is correct! \n")
+				score += 1
+				print("Your score is {}").format(score)
+			else:
+				print("Your answer is incorrect \n")
+				print("Your score is {}").format(score)
+			#A question has been attempted, increment position variable
+			position +=1
+			if position == len(questions):
+				print("Your total socre is {} \n").format(score)
+				print("Questions in module over. Please take another quiz")
+
+				#Call rank function
+				rank()
+
+				#Call list quizzes function
+				list_quizzes()
+	else:
+		print "Invalid response.Quiz does not exist."
+
+	path_to_quiz_MATH = 'C:\\Quizzler\\Quizzes\\MATH'
+	if quiz_name in os.path.basename(path_to_quiz_MATH):
+
+		#use json load function to convert to list
+		with open(path_to_quiz_MATH) as MATH_quiz:
+			MATH = json.load(MATH_quiz)
+
+		#run quiz
+		questions  = MATH.keys()
+		random.shuffle(questions)
+
+		#Start quiz
+		#Set initial score to Zero 
+		score = 0
+
+		#Monitor number of questions asked
+		position = 0
+		while position < len(questions):
+
+			#return a question in the quiz
+			print questions[position]
+
+			#Call start timing function
+			#start_timing()
+
+			#Prompt user for an answer
+			user_answer = input("Please enter your answer.\n")
+			if user_answer == MATH[(questions[position])]:
 				print("Your answer is correct! \n")
 				score += 1
 				print("Your score is {}").format(score)
@@ -227,8 +230,11 @@ def take_quiz(quiz_name):
 				print("Your total socre is {} \n").format(score)
 				print("Questions in module over.")
 
-				#Call play_agin function
+				#Call play_again function
 				play_again()
+
+	else:
+		print "Invalid response.Quiz does not exist."
 
 
 def play_again():
